66eb528d15e984cef4d583a2fadfbb7e860612ab
christinezosche <christine.zosche@gmail.com>
2020-05-31 09:46:45 -0700

    WIP: Adding initial file/folder structure

---
+++ lib/hogwarts_cli_environment.rb
@@ -0,0 +1,9 @@
+require 'bundler/setup'
+Bundler.require(:default)
+require 'open-uri'
+require 'net/http'
+
+require_relative './hogwarts_cli/cli.rb'
+require_relative './hogwarts_cli/api.rb'
+require_relative './hogwarts_cli/hogwarts_houses.rb'
+
